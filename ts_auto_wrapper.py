af959ce76189579331126fd06e11b4516db97392
Yoav Ravid <yoav.rvd@gmail.com>
2023-05-17 16:16:21 +0300

    More documentation on autowrapper

--- ts_auto_wrapper.py
+++ ts_auto_wrapper.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""Wrapper for the trisurf (new cluster_trisurf version)
+"""Automatic Wrapper for the trisurf 
 
-Has python binding to the library using CDLL and classes for the important structres (ts_vertex, ts_tape, ts_vesicle)
+use
+>>> from ts_auto_wrapper import TSWrapper
+>>> ts = TSWrapper('/path/to/trisurf/project/trisurf_ng')
+(see documentation on TSWrapper)
+
+The wrapper is rudimentary, and cannot parse complex, bracketed, or recurvsive definitions,
+especially function pointers.
+The wrapper also requires foreknowledge on the types used by the libraries, such as
+'gsl_complex' and 'xmlDocPtr'. in order to parse the headers. 
+This is handled by the dictionaries 'base_types' and 'added_types', map the names to ctypes types.
+TSWrapper can also take a dictionary for even more types.
 """
 
 ########################
@@ -11,123 +21,12 @@ Has python binding to the library using CDLL and classes for the important struc
 
 from ctypes import *
 from pathlib import Path
-from itertools import zip_longest
 import sys
 import warnings
-import os
 
 ##############################################
 # 0: Horrible homemade c parser. TODO: trash #
 ##############################################
-
-def clean_text(text):
-    """Clean text from /**/ and // comments, and empty lines"""
-    text=text.replace('\t','    ')
-    mid="/*"
-    text = text
-    while mid:
-        pre,mid,post = text.partition('/*')
-        text = pre+post.partition('*/')[-1]
-    return '\n'.join(y for x in text.splitlines() if (y:=x.partition('//')[0].strip()))
-
-
-def split_header_text_to_items(text):
-    """Parse a cleaned header file text, return list of items ('type',['lines']) for further process.
-    
-    The items are of the following types:
-    * compiler directive ('#',['#...'])
-    * enums ('enum',['enum name {',...'last=2,}'])
-    * type definitions ('typedef',['typdef void ts_void;'])
-    * structure definitions ('struct',['struct ts_square {','ts_double side;','};'])
-    * extern (global) ('extern',['extern ts_bool quiet;']) 
-    """
-    items = []
-    curr = []
-    open_char = ''
-    for i, line in enumerate(text.splitlines()):
-        # continue current item from previous open lines
-        if open_char=='{':
-            curr.append(line)
-        # Add item: we look at #directives, enums, typedefs, structures, and externs
-        if line.startswith("#"):
-            curr=[line]
-            items.append(('#',curr))
-        if line.startswith("extern"):
-            curr=[line]
-            items.append(('extern',curr))
-        if "enum" in line:
-            curr=[line]
-            items.append(('enum',curr))
-        if 'typedef struct' in line:
-            curr=[line]
-            items.append(('typedef',curr))
-            items.append(('struct',curr))
-        elif 'typedef' in line:
-            curr = [line]
-            items.append(('typedef',curr))
-        elif 'struct' in line and not open_char:
-            curr=[line]
-            items.append(('struct',curr))
-        #
-        if '{' in line:
-            open_char='{'
-        if '}' in  line:
-            open_char=''
-    return items
-
-def text_bracket_partition(text,sep=';'):
-    """Take a comment-less text of 'pre { most } post', return pre,most.split(sep),post.
-    
-    Useful for structs and enums, e.g.
-    'enum adhesion_model { a=1,b=2,};'
-    with sep=',', this will return ('enum adhesion_model',['a=1','b=2'],'')
-    'typdef struct { int g; int b;} ts_coord;'
-    this will return (typdef struct,['int g', 'int b'],'ts_coord')
-    """
-    pre, _, most = text.partition('{')
-    most,_,post = most.strip().rpartition('}')
-    most = most.split(sep)
-    most = [y for x in most if (y := x.strip(' ;{}\n'))]
-    pre = pre.strip(' \n;')
-    post = post.strip(' \n;')
-    return pre,most,post
-
-
-def parse_type(line,types):
-    """Parse a general type declaration line"""
-    first, *rest = line.replace('*',' * ').strip(' ;').split(',')
-    base_type = first.replace('*','').replace('[','').replace(']','').replace(';','').rpartition(' ')[0].strip()
-    left = first.replace(base_type,'').strip()
-    res = [left, *rest]
-    out = {}
-    for item in res:
-        if not item.strip():
-            continue # skip excess non-fields 'int a,'->'a',''
-        ty = types.get(base_type)
-        num_star=item.count('*')
-        item=item.replace('*','').strip()
-        for i in range(num_star):
-            ty = POINTER(ty)
-        while ']' in item:
-            item, _, _ = item.rpartition(']')
-            item, _, num = item.rpartition('[')
-            ty = int(num)*ty if num else POINTER(ty)
-        out[item.strip()]=ty
-    return base_type, out
-
-def parse_struct(text,types):
-    pre, most, post = text_bracket_partition(text)
-    name = ' '.join([pre,post]).strip().split()[-1].strip(' ;')
-    fields = []
-    for field in most:
-        if 'struct' in field:
-            field = field.replace('struct','').strip()
-        if not field:
-            continue
-        _, out = parse_type(field,types)
-        for item_name, item_type in out.items():
-            fields.append((item_name, item_type))
-    return name, fields
 
 # mapping from c string representatin of type "int long ..." to ctype types
 # it is critical that longer, more complete types come before simpler
@@ -188,22 +87,160 @@ added_types = {
     'xmlNodePtr' : c_void_p,
 }
 
+def clean_text(text):
+    """Clean text from /**/ and // comments, and empty lines."""
+    text=text.replace('\t','    ')
+    mid="/*"
+    text = text
+    while mid:
+        pre,mid,post = text.partition('/*')
+        text = pre+post.partition('*/')[-1]
+    return '\n'.join(y for x in text.splitlines() if (y:=x.partition('//')[0].strip()))
+
+
+def split_header_text_to_items(text):
+    """Parse a cleaned header file text, return list of items ('type',['lines']) for further process.
+    
+    The items are of the following types:
+    * compiler directive ('#',['#...'])
+    * enums ('enum',['enum name {',...'last=2,}'])
+    * type definitions ('typedef',['typdef void ts_void;'])
+    * structure definitions ('struct',['struct ts_square {','ts_double side;','};'])
+    * extern (globals) ('extern',['extern ts_bool quiet;']) 
+    """
+    items = []
+    curr = []
+    open_char = ''
+    for i, line in enumerate(text.splitlines()):
+        # continue current item from previous open lines
+        if open_char=='{':
+            curr.append(line)
+        # Add item: we look at #directives, enums, typedefs, structures, and externs
+        if line.startswith("#"):
+            curr=[line]
+            items.append(('#',curr))
+        if line.startswith("extern"):
+            curr=[line]
+            items.append(('extern',curr))
+        if "enum" in line:
+            curr=[line]
+            items.append(('enum',curr))
+        if 'typedef struct' in line:
+            curr=[line]
+            items.append(('typedef',curr))
+            items.append(('struct',curr))
+        elif 'typedef' in line:
+            curr = [line]
+            items.append(('typedef',curr))
+        elif 'struct' in line and not open_char:
+            curr=[line]
+            items.append(('struct',curr))
+        #
+        if '{' in line:
+            open_char='{'
+        if '}' in  line:
+            open_char=''
+    return items
+
+def text_bracket_partition(text,sep=';'):
+    """Take a comment-less text of 'pre { most } post', return pre,most.split(sep),post.
+    
+    Useful for structs and enums, e.g.
+    >>> text_bracket_partition('enum adhesion_model { a=1,b=2,};', sep=',')
+    ('enum adhesion_model', ['a=1','b=2'], '')
+    >>> text_bracket_partition('typdef struct { int g; int b;} ts_coord;')
+    ('typdef struct', ['int g', 'int b'], 'ts_coord')
+    """
+    pre, _, most = text.partition('{')
+    most,_,post = most.strip().rpartition('}')
+    most = most.split(sep)
+    most = [y for x in most if (y := x.strip(' ;{}\n'))]
+    pre = pre.strip(' \n;')
+    post = post.strip(' \n;')
+    return pre,most,post
+
+
+def parse_type(line,types):
+    """Parse a general type declaration 'type0 type1 *a,b,c[];' to base_type and name-type doctionray.
+    
+    Rudimentary: this is not capable of parsing parenthesis (especially function pointer) 
+    or multi-level declarations.
+    >>> parse_type('long int*unsigned, b[3], c', base_types)
+    (long int, {'unsigned': ctypes.LP_c_long, 'b': ctypes.c_long_Array_3, 'c': ctypes.c_long})
+    """
+    first, *rest = line.replace('*',' * ').strip(' ;').split(',')
+    base_type = first.replace('*','').replace('[','').replace(']','').replace(';','').rpartition(' ')[0].strip()
+    left = first.replace(base_type,'').strip()
+    res = [left, *rest]
+    out = {}
+    for item in res:
+        if not item.strip():
+            continue # skip excess non-fields 'int a,'->'a',''
+        ty = types.get(base_type)
+        num_star=item.count('*')
+        item=item.replace('*','').strip()
+        for i in range(num_star):
+            ty = POINTER(ty)
+        while ']' in item:
+            item, _, _ = item.rpartition(']')
+            item, _, num = item.rpartition('[')
+            ty = int(num)*ty if num else POINTER(ty)
+        out[item.strip()]=ty
+    return base_type, out
+
+def parse_struct(text,types):
+    """Parse a struct "... s_name {ty0 field0; ty1 field1;...}" to name and fields [(name, type)].
+    
+    This is used for constructing ctypes Structures._fields_
+    e.g.
+    >>> parse_struct('typdef struct { int g; int b;} ts_coord;',base_types)
+    ('ts_coord', [('g', ctypes.c_int),('b', ctypes.c_int)])
+    """
+    pre, most, post = text_bracket_partition(text)
+    name = ' '.join([pre,post]).strip().split()[-1].strip(' ;')
+    fields = []
+    for field in most:
+        if 'struct' in field:
+            field = field.replace('struct','').strip()
+        if not field:
+            continue
+        _, out = parse_type(field,types)
+        for item_name, item_type in out.items():
+            fields.append((item_name, item_type))
+    return name, fields
+
+
 class TSWrapper():
     """Class that instantiate a trisurf wrapper from a path.
     
     Design to act like a module i.e.
-    >>> import wrapper as ts
-    becomes
-    >>> from autowrapper import TSWrapper
-    >>> ts = TSWrapper(path_to_trisurf_lib)
+    >>> from ts_auto_wrapper import TSWrapper
+    >>> ts = TSWrapper('/path/to/trisurf/project/trisurf_ng')
+    Exposes python binding to the library using CDLL.
+    Everything is available in ts.X, but they are also organized by types with faster autocompletes:
+        ts.ts_types: classes for types (ts_vertex, ts_tape, ts_vesicle, ...)
+        ts.functions: functions (init_vertex, parseDump, ...)
+        ts.globals: global variables
+        ts.enums: enum definitions
+        ts.TS_...: several #defines are hardcoded (e.g. TS_SUCCESS)
+    and misc. things: ctype function POINTER, pointer, pretty_print, and byte_to_int
+
+    The raw functions with 'restype' and 'argtype' are available as in ts.functions._c_functionname
+    The CDLL itself is available in ts.cdll
+
+    The wrapper also requires foreknowledge on the names and types used by the libraries, such as
+    'gsl_complex' and 'xmlDocPtr'. TSWrapper can accept a dictionary for more types {'name': ctypes.structure...}.
     """
     
-    def __init__(self,path_to_trisurf='/opt/workspace/msc_project/cluster-trisurf'):
+    def __init__(self,path_to_trisurf='/opt/workspace/msc_project/cluster-trisurf', more_types=None):
+        """Initializing the wrapper: Roughly the equivalent of a wrapper module code, indented twice."""
         
         # add module functions:
         self.clean_text = clean_text
         self.base_types = base_types
         self.added_types = added_types
+        if more_types:
+            self.added_types = added_types.update(more_types)
         self.split_header_text_to_items = split_header_text_to_items
         self.text_bracket_partition = text_bracket_partition
         self.parse_struct = parse_struct
@@ -224,7 +261,7 @@ class TSWrapper():
             with open(header,'r') as f:
                 text=f'{text}\n{f.read()}'
         self.text=clean_text(text)
-        self.items = split_header_text_to_items(clean_text(text))
+        self.items = split_header_text_to_items(self.text)
         #############################################################
         # 1: parse general.h for definitions, enums, and structures #
         #############################################################
@@ -297,26 +334,20 @@ class TSWrapper():
         # 3: load the trisurf library #
         ###############################
 
-        self.ts=CDLL(self.path_to_trisurf_library)
+        self.cdll=CDLL(self.path_to_trisurf_library)
 
         # # globals
-        # global_command_line_args = POINTER(ts_args).in_dll(ts,"command_line_args")
-        # global_quiet = ts_bool.in_dll(ts,"quiet")
-        # global_V0 = ts_double.in_dll(ts,"V0")
-        # global_A0 = ts_double.in_dll(ts,"A0")
-        # global_epsvol = ts_double.in_dll(ts,"epsvol")
-        # global_epsarea = ts_double.in_dll(ts,"epsarea")
-        self.global_vars = {}
+        self.globals = {}
         for item in filter(lambda x:x[0]=='extern',self.items):
             pre,most,post = text_bracket_partition("\n".join(item[1]))
             defline = ' '.join((pre,post)).strip() # typedef [type...] name
             ty = ' '.join(defline.split()[1:-1]) if 'inline' not in defline else 'inline'
             name = defline.split()[-1].strip(' ;')
             if ty!='inline' and ')' not in name:
-                self.global_vars[name]=self.ts_types[ty].in_dll(self.ts,name)
-                self.__dict__[name]=self.global_vars[name] # all globals are available in namespace
+                self.globals[name]=self.ts_types[ty].in_dll(self.cdll,name)
+                self.__dict__[name]=self.globals[name] # all globals are available in namespace
 
-        self.funcs = {}
+        self.dec_funcs = {}
         text=(self.text.replace(' extern ',' ').replace(' inline ',' ').replace(' const ',' ')
                        .replace('\nextern ','\n').replace('\ninline ','\n').replace('\nconst ','\n')
                        .replace(';extern ',';').replace(';inline ',';').replace(';const ',';')
@@ -341,10 +372,11 @@ class TSWrapper():
                     if line:
                         errstr = f'{line_start}({args}){text.partition(" ")[0]}'
                         raise ValueError(f'{errstr} parsed an extra {line} item on top of {name}:{ty}')
-                    self.funcs[name]=(tyname,ty,args)
+                    self.dec_funcs[name]=(tyname,ty,args)
         
         class ts_functions: 
-            pass
+            def __getitem__(self,s):
+                return self.__getattribute__(s)
         self.functions=ts_functions()
         ts_success = self.ts_types['ts_bool'](self.TS_SUCCESS).value
         ts_fail = self.ts_types['ts_bool'](self.TS_FAIL).value
@@ -357,11 +389,11 @@ class TSWrapper():
         def process_double_ref_to_doube(arg): return arg.contents.value;
 
         try:
-            for func, (tyname, ty, args) in self.funcs.items():
+            for func, (tyname, ty, args) in self.dec_funcs.items():
                 signature=[parse_type(y,self.ts_types) for x in args.split(',') if (y:=x.strip())]
                 signature_name_type = [list(x[1].items())[0] for x in signature]
                 signature_type = [x[1] for x in signature_name_type]
-                self.functions.__dict__["_c_"+func] = _c_f = self.ts[func]
+                self.functions.__dict__["_c_"+func] = _c_f = self.cdll[func]
                 self.functions.__dict__["_c_"+func].argtype=signature_type
                 self.functions.__dict__["_c_"+func].restype=ty
                 signature_str= ""
@@ -380,7 +412,7 @@ class TSWrapper():
                         process_out.append(None)
                         arg_types.append(arg_type)
                         signature_str = f'{signature_str}, {arg_name}: bool'
-                    elif arg_type==c_char_p:
+                    elif arg_type==c_char_p or arg_type==POINTER(c_char):
                         process_args.append(process_str_to_char_p)
                         process_out.append(None)
                         arg_types.append(arg_type)
@@ -413,43 +445,15 @@ class TSWrapper():
                     return all_ret
                 f.__doc__=doc
                 self.functions.__dict__[func]=f
+                self.__dict__[func]=f
         except (RuntimeError,IndexError,ValueError,KeyError) as e:
             print(e)
+    
 
-    ########################
-    # 4: function wrappers #
-    ########################
+    ################################
+    # 4: special function wrappers #
+    ################################
 
-
-    def parsetape(self,filename='tape'):
-        """Loads tape with  filename (if not given it defaults to 'tape'). It returns a pointer to structure for tape"""
-        self.ts.parsetape.restype=POINTER(self.ts_tape)
-        self.ts.parsetape.argtypes=[c_char_p]
-        return self.ts.parsetape(filename.encode('ascii'))
-
-    def parseDump(self,filename):
-        """Loads a vtu file with 'filename' and creates a vesicle returning pointer to it"""
-        self.ts.parseDump.argtypes=[c_char_p]
-        self.ts.parseDump.restype=POINTER(self.ts_vesicle)
-        vesicle=self.ts.parseDump(filename.encode('ascii'))
-        return vesicle
-
-    def single_timestep(self,vesicle):
-        """Makes a single timestep in simulations. Returns a tuple of vmsrt and bfrt (vertex move success rate and bond flip success rate)"""
-        self.ts.single_timestep.argtypes=[POINTER(self.ts_vesicle),POINTER(c_double),POINTER(c_double)]
-        vmsrt=c_double(0.0)
-        bfsrt=c_double(0.0)
-        self.ts.single_timestep(vesicle,byref(vmsrt),byref(bfsrt))
-        return (vmsrt.value, bfsrt.value)
-
-
-    def gyration_eigen(self,vesicle):
-        ts.gyration_eigen.argtypes=[POINTER(self.ts_vesicle), POINTER(c_double), POINTER(c_double), POINTER(c_double)]
-        l1=c_double(0.0)
-        l2=c_double(0.0)
-        l3=c_double(0.0)
-        self.ts.gyration_eigen(vesicle , byref(l1), byref(l2), byref(l3))
-        return (l1.value, l2.value, l3.value)
 
     def vertex_adhesion(self,vesicle,vtx):
         """energy of a single vertex: piece of adhesion_energy_diff"""
@@ -471,6 +475,7 @@ class TSWrapper():
             return (field, thing_c)
  
     def pretty_text(self,thing_c):
+        """Nicer string representation of a ctypes struct, like LP_ts_tape"""
         stuff=[]
         ret=''
         if 'contents' in dir(thing_c):
